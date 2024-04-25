e68b28beab92c9931d0cd7340451cfa943800639
Reihane Montazeri <rmontazeri82@gmail.com>
2024-04-24 19:55:28 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿let y
+let x
+let context_Menu = document.querySelector('#contextMenu')
+
+document.body.addEventListener('contextmenu', function(event){
+    event.preventDefault()
+    if (context_Menu.style.display == 'none'){
+        x = 
+        y = 
+        context_Menu.style.display = 'block'
+        context_Menu.style.top = event.pageY + 'px'
+        context_Menu.style.left = event.pageX + 'px'
+    }else{
+        context_Menu.style.top = event.pageY + 'px'
+        context_Menu.style.left = event.pageX + 'px'
+    }
+})
+
+document.body.addEventListener('click', function(event){
+    context_Menu.style.display = 'none'
+})
+
+document.body.addEventListener('keydown', function(event){
+    if (event.keyCode === 27){
+        context_Menu.style.display = 'none'
+    }
+})
